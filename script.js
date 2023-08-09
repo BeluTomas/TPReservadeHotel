aebc0de2b32bb22d5bd741e22fe7d05cae8336a6
BeluTomas <137287274+BeluTomas@users.noreply.github.com>
2023-08-09 20:13:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-button').onclick = () =>{
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.contact .row .faq .box h3').forEach(faqBox => {
+   faqBox.onclick = () =>{
+      faqBox.parentElement.classList.toggle('active');
+   }
+});
+
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   effect: "coverflow",
+   spaceBetween: 30,
+   grabCursor: true,
+   coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+
+var swiper = new Swiper(".gallery-slider", {
+   loop:true,
+   effect: "coverflow",
+   slidesPerView: "auto",
+   centeredSlides: true,
+   grabCursor: true,
+   coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+   },
+   pagination: {
+      el: ".swiper-pagination",
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   slidesPerView: "auto",
+   grabCursor: true,
+   spaceBetween: 30,
+   pagination: {
+      el: ".swiper-pagination",
+   },
+   breakpoints: {
+      768: {
+        slidesPerView: 1,
+      },
+      991: {
+        slidesPerView: 2,
+      },
+   },
+});
+$(document).ready(function(){
+    
+    $(".contenedor-formularios").find("input, textarea").on("keyup blur focus", function (e) {
+
+        var $this = $(this),
+          label = $this.prev("label");
+
+        if (e.type === "keyup") {
+            if ($this.val() === "") {
+                label.removeClass("active highlight");
+            } else {
+                label.addClass("active highlight");
+            }
+        } else if (e.type === "blur") {
+            if($this.val() === "") {
+                label.removeClass("active highlight"); 
+                } else {
+                label.removeClass("highlight");   
+                }   
+        } else if (e.type === "focus") {
+            if($this.val() === "") {
+                label.removeClass("highlight"); 
+            } 
+            else if($this.val() !== "") {
+                label.addClass("highlight");
+            }
+        }
+
+    });
+
+    $(".tab a").on("click", function (e) {
+
+        e.preventDefault();
+
+        $(this).parent().addClass("active");
+        $(this).parent().siblings().removeClass("active");
+
+        target = $(this).attr("href");
+
+        $(".contenido-tab > div").not(target).hide();
+
+        $(target).fadeIn(600);
+
+    });
+    function showRegistrationForm() {
+      document.getElementById("registrarse").style.display = "block";
+}
+function showRegistrationForm() {
+  document.getElementById("registrationForm").style.display = "block";
+}
+
+    
+});
